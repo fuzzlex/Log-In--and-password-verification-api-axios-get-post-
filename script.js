37fb26a8e3397f6b1cb97338fa426c38082da9f0
emre <emrky.2018@icloud.com>
2021-11-27 11:27:53 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,68 @@
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const submitButton = document.querySelector(".submitt input");
+const container = document.querySelector(".container");
+const newpage = document.querySelector(".new-page")
+
+window.onload = ()=>{
+    setTimeout(setUsernamePass, 1000)
+}
+
+const setUsernamePass = () =>{
+    username.value = "eve.holt@reqres.in"
+    password.value = "pistol"
+    localStorage.setItem("username",  EncryptStringAES(username.value))
+    localStorage.setItem("password",  EncryptStringAES(password.value))
+
+}
+
+submitButton.addEventListener("click", (e)=>{
+    e.preventDefault();
+    apisentRegister();
+
+
+})
+
+const apisentRegister = async () =>{
+    const dataBody = {
+        username : username.value,
+        password : password.value
+    }
+    const response = await axios({
+        url : "https://reqres.in/api/register",
+        data : dataBody,
+        method : "post"
+    })
+    const {data} = response
+    if(data.token == undefined){
+        alert(response.error)
+    }
+    else{
+        localStorage.setItem("apikey", EncryptStringAES(data.token))
+        container.remove();
+        newpage.style.display = "block"
+        getapiSource()
+    }
+}
+
+const getapiSource = async () =>{
+    const response = await axios({
+        url : "https://reqres.in/api/users?page=1",
+    })
+    const {data} = response;
+    data.data.forEach(element => {
+        newpage.innerHTML +=  ` 
+        <ul class="newpage-inner" >
+        <li >${element.id}</li>  
+        <li>${element.email}</li>
+        <li>${element.first_name} ${element.last_name}</li>
+        <li><img src="${element.avatar}"/></li>
+     
+    
+ </ul>
+ 
+ `
+        
+    });
+   
+}
